fb6b14f7e4df7b53506febdc1901bb77eba7f739
Brndndev <brndndev@outlook.com>
2021-11-23 20:03:41 -0500

    🔵 ran test for variable suitCounter: pass ✅

---
+++ 52cards.js
@@ -0,0 +1,13 @@
+// I want to use a loop to tell the computer all the possible suites and ranks that exisit
+
+let  suits = ['H', 'C', 'D', 'S'];
+//the single quote will help javascript treat them as text-based strings
+let ranks = ['2' , '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A'];
+//the "=" sign serves as the assignment operater
+//the single quotes will tell javascript to treat the numbers as text-based strings and not numbers
+
+for(let suitCounter = 0; suitCounter< 4; suitCounter++) {
+    console.log(suitCounter)
+}
+//---        🃏  this is my test to make sure my variable met the conditions of <4 and added 1
+
--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # deck of cards for a poker game...coming soon 🃏
+
+A reg deck contains up to 52 cars
+
+there are 2 suites 
+
+♥️ - Hearts
+
+♣️ - Clubs
+
+♦️ - Diamonds
+
+♠️ - Spades
+
+13 ranks of each suite [ace, 2, 3, 4, 5, 6, 7, 8, 9, 10, Jack, Queen, King & Joker]
+
+4 suites x 13 ranks
+
+using a LOOP we can communicate with the computer what the possible suites and ranks are
+
+We need a loop for the suites
+
+and
+
+We need a loop for the ranks
